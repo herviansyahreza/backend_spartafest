6fd2234828587ed3e3702a808f6d6a3c8d294091
herviansyahreza <reza.herviansyah99@gmail.com>
2022-12-17 00:15:17 +0700

    Update validation.js

--- validators/validation.js
+++ validators/validation.js
@@ -17,20 +17,19 @@ const login = [
 const lombakelompok = [
     body(`nama_tim`).notEmpty(),
     body(`ketua`).isLength({min:6}).isString().notEmpty(),
-    body(`kontak`).isInt().isLength({min:12}).notEmpty(),
+    body(`kontak`).isLength({min:12}).notEmpty(),
     validator
 ]
 const ketua = [
-    body(`nama_satu`).notEmpty(),
-    body(`nim_satu`).isLength({min:12}).notEmpty(),
-    body(`email_satu`).isEmail().notEmpty(),
+    body(`ketua`).notEmpty(),
+    body(`nim`).notEmpty(),
+    body(`email`).isEmail().notEmpty(),
     validator 
 ]
 const anggotasatu = [
-    body(`ketua`).notEmpty(),
-    body(`nim`).isLength({min:12}).notEmpty(),
-    body(`email`).isEmail().notEmpty(),
-    body(`ketua`).notEmpty(),
+    body(`nama_satu`).notEmpty(),
+    body(`nim_satu`).notEmpty(),
+    body(`email_satu`).isEmail().notEmpty(),
     validator
 ]
 const anggotadua = [
